a624dc16bbaf9bb90eec3c500b9c99eea0f8b0c9
Junaid-24 <82566461+Junaid-24@users.noreply.github.com>
2022-12-08 14:21:30 +0500

    Add files via upload

---
+++ Admin/src/com/mycompany/cattlenetic/CattleNetic.java
@@ -0,0 +1,9 @@
+package com.mycompany.cattlenetic;
+
+public class CattleNetic {
+
+    public static void main(String[] args) {
+        FarmSystem f1 = new FarmSystem();
+        f1.setVisible(true);
+    }
+}
